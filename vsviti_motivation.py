4091ef56a8f71141b31835c79d666944487cc4c8
chamois1 <nrabicenko5@gmail.com>
2023-07-30 16:08:55 +0300

    Update parse

--- vsviti_motivation.py
+++ vsviti_motivation.py
@@ -1,13 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 URL = "https://vsviti.com.ua/society/81550"
 
 response = requests.get(URL)
 soup = BeautifulSoup(response.text, "lxml")
 
-block_quotes = soup.find("div", class_="td-post-content")
-for quotes in block_quotes:
-    quotes = quotes.get_text(strip=True)
+quotes = soup.find_all("p")
+quote_list = []
 
-    print(quotes)
+for q in quotes:
+    qoute = q.get_text()
+
+    dict_qoutes = {
+        qoute[:2]: qoute[2:]
+    }
+
+    quote_list.append(dict_qoutes)
+
+
+with open("result_quotes.json", "w", encoding="utf-8") as json_file:
+    json.dump(quote_list, json_file, ensure_ascii=False)
